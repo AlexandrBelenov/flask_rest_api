2ce449699ec3850ce348867d00b4e0af540d988c
Alexander Belenov <AlexanderBelenov@github.com>
2020-07-01 00:14:23 +0300

    Added two dictionaries

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+[
+    {
+        'title': 'video #1. Intro',
+        'description': 'GET, POST routes'
+    },
+    {
+        'title': 'video #2. More features',
+        'description': 'GET, POST routes'
+    }
+]
