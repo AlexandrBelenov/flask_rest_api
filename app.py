865183cb247e723d31789425d0ed80166a2080c1
Alexander Belenov <AlexanderBelenov@github.com>
2020-07-01 00:36:21 +0300

    Added simple routes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+client = app.test_client()
 
-[
+tutorials = [
     {
         'title': 'video #1. Intro',
         'description': 'GET, POST routes'
@@ -12,3 +13,19 @@ app = Flask(__name__)
         'description': 'GET, POST routes'
     }
 ]
+
+
+@app.route('/tutorials', methods=["GET"])
+def get_list():
+    return jsonify(tutorials)
+
+
+@app.route('/tutorials', methods=["POST"])
+def update_list():
+    data = request.json
+    tutorials.append(data)
+    return jsonify(tutorials)
+
+
+if __name__ == "__main__":
+    app.run()
